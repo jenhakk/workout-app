2ea3465abc9d13cf035625a184bc074d40edd5a3
amakarj <amanda.karjalainen@hotmail.com>
2022-09-29 13:05:51 +0300

    Added ViewMeasAdd.js

---
+++ workoutapp/views/ViewMeasAdd.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import {Button, Text, Input} from '@rneui/base';
+
+const ViewMeasAdd = () => {
+  const [meas, setMeas] = useState([
+    'Weight',
+    'Chest',
+    'Bicep',
+    'Waist',
+    'Hip',
+    'Thigh',
+  ]);
+
+  const keyHandler = (item, index) => {
+    return index.toString();
+  };
+
+  return (
+    <View style={styles.measContainer}>
+      <View style={styles.measWrapLists}>
+        <FlatList
+          data={meas}
+          keyExtractor={keyHandler}
+          renderItem={item => {
+            return (
+              <View>
+                <Text style={styles.measLabels}>
+                  {item.index + 1}. {item.item}:
+                </Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+      <View style={styles.wrapMeasLists}>
+        <FlatList
+          data={meas}
+          keyExtractor={keyHandler}
+          renderItem={item => {
+            return (
+              <View>
+                <Input
+                  inputContainerStyle={styles.measInput}
+                  inputStyle={styles.measInputText}
+                  placeholder={item.item}
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  measContainer: {
+    marginVertical: 40,
+    width: '80%',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+  },
+  measWrapLists: {
+    flexDirection: 'column',
+  },
+  measLabels: {
+    color: 'grey',
+    fontSize: 16,
+    fontWeight: 'normal',
+    paddingBottom: 40,
+  },
+  measInput: {
+    width: 100,
+    height: 35,
+  },
+  measInputText: {
+    fontSize: 16,
+  },
+});
+
+export default ViewMeasAdd;
